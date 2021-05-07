abd8cb4c163c335a587c3082c97de9a173803bde
DESKTOP-G34FBF1\blank <blanknhide@naver.com>
2021-05-07 19:15:35 +0900

    [+] Update Warning

--- src/main/java/Util/Hash.java
+++ src/main/java/Util/Hash.java
@@ -7,17 +7,17 @@ public class Hash
 {
     public static String getMd5(String value)
     {
-        String result = "";
+        String result;
         try
         {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(value.getBytes());
-            byte byteData[] = md.digest();
+            byte[] byteData = md.digest();
             StringBuffer sb = new StringBuffer();
 
-            for (int i = 0 ; i < byteData.length ; i++)
+            for (byte data : byteData)
             {
-                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+                sb.append(Integer.toString((data & 0xff) + 0x100, 16).substring(1));
             }
             result = sb.toString();
         } catch (NoSuchAlgorithmException e)
