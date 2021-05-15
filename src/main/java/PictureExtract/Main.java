cfdeb4eac42fc1f0f23f13457770d0062132dd4b
DESKTOP-G34FBF1\blank <blanknhide@naver.com>
2021-05-15 16:42:17 +0900

    [+] Update args

--- src/main/java/PictureExtract/Main.java
+++ src/main/java/PictureExtract/Main.java
@@ -5,7 +5,8 @@ public class Main
 
     public static void main(String[] args)
     {
-        PictureHandler  picturehandler = new PictureHandler("./Resource/test.jpg", "powerprove", 1);
+        //PictureHandler  picturehandler = new PictureHandler("./Resource/test.jpg", "powerprove", 1);
+        PictureHandler  picturehandler = new PictureHandler(args[0], args[1], Integer.parseInt(args[2]));
         picturehandler.ExtractFullMetaData();
         picturehandler.ExtractCoreMetaData();
         System.out.println(picturehandler);
